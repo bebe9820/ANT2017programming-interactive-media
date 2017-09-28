dd925e1d23c65bbbac8fa376b5e95257fd11f7d5
bebe9820 <32293596+bebe9820@users.noreply.github.com>
2017-09-28 17:34:32 +0900

    Add files via upload
    눈 내리는 파일

--- sketch.js
+++ sketch.js
@@ -1,10 +1,44 @@
+var cx = [];
+var cy = [];
+var csize =[];
+var N, i;
+
 function setup() {
-createCanvas(windowWidth, windowHeight);
-background(255,0,0);
+	createCanvas(windowWidth, windowHeight);
+		initCircles(); 
 }
+
 function draw() {
-if (mouseIsPressed) {
-var size = random(10, 100);
-ellipse(mouseX, mouseY, size, size);
+	background(124);
+
+	var c = color(255);
+	drawCircles(c);
+
+}
+
+function initCircles() {
+	N = 100;
+	for (i=0; i<N; i++) {
+		cx[i] = random(0, width);
+		cy[i] = random(0, height);
+		csize[i] = random(5,20);
+} 
+}
+
+function drawCircles( c ) {
+	fill(255);
+	stroke(c, 50);
+	strokeWeight(3);
+
+for (i=0; i<N; i++) {
+	cx[i] = cx[i] + random(0);
+	cy[i] = cy[i] + random(10);
+
+	if(cy[i]>height)
+	cy[i]=0;
+
+	ellipse(cx[i], cy[i], csize[i], csize[i]);
 }
 }
+
+//draw와 setup 잘 구분해서 하기 
